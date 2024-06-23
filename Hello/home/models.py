5da845a389357f35dc66c647f04b6019af2179e5
amitdash1 <superiorcrew90@gmail.com>
2024-06-23 13:59:18 +0530

    first commit

---
+++ Hello/home/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+
+# Create your models here.
+class Contact(models.Model):
+    name = models.CharField(max_length=122)
+    email = models.CharField(max_length=122)
+    phone = models.CharField(max_length=12)
+    desc = models.TextField()
+    date = models.DateField()
+
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    stock = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+    
